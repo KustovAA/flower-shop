05a71629441e9275ec6395542341c57e916dcc5c
Norgius <dmitry.schadrin21@yandex.ru>
2023-01-18 23:35:32 +0300

    Add json and its parse

---
+++ populate_db_with_data.py
@@ -0,0 +1,24 @@
+import json
+
+
+def save_bouquet(raw_bouquet):
+    bouquet, created = Bouquet.objects.get_or_create(
+        title=raw_bouquet.get('title'),
+        price=raw_bouquet.get('price'),
+        flowers=raw_bouquet.get('flowers'),
+        size=raw_bouquet.get('size'),
+        description=raw_bouquet.get('description'),
+        picture=raw_bouquet.get('link')
+    )
+
+
+def main():
+    with open('bouquets.json', 'r') as file:
+        bouquets = json.loads(file.read())
+
+    for bouquet in bouquets:
+        save_bouquet(bouquet)
+
+
+if __name__ == '__main__':
+    main()
