387d22c5e16519b9d13312365669b0cfa1d2662c
Norgius <dmitry.schadrin21@yandex.ru>
2023-01-19 21:19:49 +0300

    Add comand populate_db

---
+++ app/flower_app/management/commands/populate_db.py
@@ -0,0 +1,27 @@
+import json
+
+from django.core.management.base import BaseCommand
+from app.flower_app.models import Bouqet
+
+
+class Command(BaseCommand):
+    help = 'Populate DB with data'
+
+    def add_arguments(self, parser):
+        parser.add_argument('file', type=str, help='File path',
+                            default='app/flower_app/bouquets.json')
+
+    def handle(self, *args, **kwargs):
+        file = kwargs.get('file')
+        with open(file, 'r') as file:
+            raw_bouquets = json.loads(file.read())
+
+        for raw_bouquet in raw_bouquets:
+            bouquet, created = Bouqet.objects.get_or_create(
+                title=raw_bouquet.get('title'),
+                price=raw_bouquet.get('price'),
+                flowers=raw_bouquet.get('flowers'),
+                size=raw_bouquet.get('size'),
+                description=raw_bouquet.get('description'),
+                picture=raw_bouquet.get('link')
+            )
