3e043d368ea802d393609018b60a4144192d6679
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2023-01-21 00:57:02 +0300

    fix deploy step

--- app/flower_app/urls.py
+++ app/flower_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('consultation', views.consultation, name='consultation'),
     path('card', views.card, name='card'),
     path('order', views.order, name='order'),
+    path('order-test', views.order, name='order-test'),
     path('order-step', views.order_step, name='order-step'),
     path('quiz', views.quiz, name='quiz'),
     path('quiz-step', views.quiz_step, name='quiz-step'),
