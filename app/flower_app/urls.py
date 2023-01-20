9164ccf252f3aa4c024f6d89e0a7fafe6dc94418
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2023-01-20 19:31:30 +0300

    add frontend

---
+++ app/flower_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('catalog', views.catalog, name='catalog'),
+    path('consultation', views.consultation, name='consultation'),
+    path('card', views.card, name='card'),
+    path('order', views.order, name='order'),
+    path('order-step', views.order_step, name='order-step'),
+    path('quiz', views.quiz, name='quiz'),
+    path('quiz-step', views.quiz_step, name='quiz-step'),
+    path('result', views.result, name='result'),
+]
--- app/flower_app/views.py
+++ app/flower_app/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+def catalog(request):
+    return render(request, 'catalog.html', {})
+
+
+def consultation(request):
+    return render(request, 'consultation.html', {})
+
+
+def card(request):
+    return render(request, 'card.html', {})
+
+
+def order(request):
+    return render(request, 'order.html', {})
+
+
+def order_step(request):
+    return render(request, 'order-step.html', {})
+
+
+def quiz(request):
+    return render(request, 'quiz.html', {})
+
+
+def quiz_step(request):
+    return render(request, 'quiz-step.html', {})
+
+
+def result(request):
+    return render(request, 'result.html', {})
